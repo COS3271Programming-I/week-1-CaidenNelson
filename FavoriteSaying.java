c5fe8665315dba8f75784c52191696a20cec1168
CaidenNelson <cwnelson4@students.unwsp.edu>
2025-01-24 10:04:25 -0600

    Create FavoriteSaying.java

---
+++ FavoriteSaying.java
@@ -0,0 +1,8 @@
+package looker;                                                                          
+                                                                                         
+public class FavoriteSaying {                                                            
+	public static void main(String[] args) {                                             
+		System.out.format("%60s","\tLoyalty is its own reward\n");                       
+	}                                                                                    
+}                                                                                        
+                                                                                         

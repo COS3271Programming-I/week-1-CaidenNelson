5526b49cd49a0b2fd7852b9dca76a643ccc77a22
CaidenNelson <cwnelson4@students.unwsp.edu>
2025-01-24 10:05:00 -0600

    Create TextArt.java

---
+++ TextArt.java
@@ -0,0 +1,27 @@
+package looker;
+
+public class Art {
+public static void main(String[] args) {
+	System.out.println("            ###############            ");
+	System.out.println("         #####################         ");
+	System.out.println("      ###########################      ");
+	System.out.println("    ##############################     ");
+	System.out.println("  #################################    ");
+	System.out.println("####################################   ");
+	System.out.println("#######      ###########       ######  ");
+	System.out.println("#######  ##  ###########  ###  ########");
+	System.out.println("#######      ###########       ########");
+	System.out.println("#######################################");
+	System.out.println("#######################################");
+	System.out.println("#######################################");
+	System.out.println("#######################################");
+	System.out.println("##########                   ##########");
+	System.out.println("###########                 ###########");
+	System.out.println("##############            #############");
+	System.out.println(" ####################################  ");
+	System.out.println("  #################################    ");
+	System.out.println("     ############################      ");
+	System.out.println("#######################################");
+	System.out.println("#######################################");
+}
+}
